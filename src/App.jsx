8d26503cc7de7e059c9f28d230da62ce3f8f858f
siddharthdubey52 <dubeysiddharth368@gmail.com>
2025-12-05 13:05:58 +0530

    counter done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ClassBased from './Component/ClassBased'
 import FunctionBased from './Component/FunctionBased'
 import Demo from './State/Demo'
+import Counter from './State/Counter'
 
 const App = () => {
   return (
     <div>
    {/* <ClassBased/> */}
    {/* <FunctionBased/> */}
-   <Demo/>
+   {/* <Demo/> */}
+   <Counter/>
     </div>
    
   )
